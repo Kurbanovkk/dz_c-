f1e82d5105b695b84788eab4d2a57751ce0ea2ce
Kurban <kurbanovkk96@mail.ru>
2022-07-10 15:06:50 +0300

    dz4

---
+++ dz4/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.*/
+
+System.Console.WriteLine("Эта программа позволяет находить четные числа от 1 до введенного, введем N: ");
+int value = int.Parse(Console.ReadLine());
+int count = 1;
+
+while (value >= count)
+{
+    if (count % 2 == 0)
+    {
+        System.Console.WriteLine($"Четное число: " + count);
+    }
+
+    else
+    {
+        System.Console.WriteLine($"Нечетное число: " + count);
+    }
+    count++;
+}
+
+// сделал еще и нечетные, но не смог упорядочить
