bf9ee288c222fa33da99bd5eb01f0c3efe5bb4b4
Kurban <kurbanovkk96@mail.ru>
2022-07-13 11:24:40 +0300

    sem2

---
+++ Seminar2_dz1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа.*/
+
+System.Console.WriteLine("Введите любое число: ");
+string value1 = Console.ReadLine();
+int index = value1.Length;
+if (index >= 2)
+{
+    var value2 = value1.ToString()[1];
+    System.Console.WriteLine(value2);
+}
+else
+{
+    System.Console.WriteLine("Второго числа нет");
+}
+
