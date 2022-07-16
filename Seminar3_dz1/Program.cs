0d54a50f3dcc415ef03e7c522e4ce265e1b26054
Kurban <kurbanovkk96@mail.ru>
2022-07-16 18:40:30 +0300

    Sem3dz1

---
+++ Seminar3_dz1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите пятизначное число: ");
+string palindrom = Console.ReadLine();
+
+/*if (palindrom[0] == palindrom[4] && palindrom[1] == palindrom[3])
+    Console.WriteLine("Ура это палиндром");
+else
+    System.Console.WriteLine("Это не палиндром");*/
+
+
+void Pali(string str)
+{
+    if (str[0] == str[4] && str[1] == str[3])
+        Console.WriteLine("Ура это палиндром!!!");
+    else
+        Console.WriteLine("Это не палиндром");
+
+}
+
+Pali(palindrom);
