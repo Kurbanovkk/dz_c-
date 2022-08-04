5b826c8148ca40e19913f8eec39248f64e6a3ad2
Kurban <kurbanovkk96@mail.ru>
2022-08-04 17:09:43 +0300

    Sem4dz2

--- Seminar4_dz2/Program.cs
+++ Seminar4_dz2/Program.cs
@@ -7,16 +7,6 @@
 System.Console.Write(" Введите число: ");
 int value = int.Parse(Console.ReadLine());
 
-int Sum(int value)
-{
-    if (value < 10)
-        return value;
-
-    return value % 10 + Sum(value / 10);
-}
-int result = Sum(value);
-System.Console.WriteLine(result);
-
 int Foo(int value)
 {
     int result = 0;
