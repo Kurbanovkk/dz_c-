c6cb020e016f0f29f68fa8e0625db30e4f6afcaa
Kurban <kurbanovkk96@mail.ru>
2022-08-09 14:53:40 +0300

    Sem5dz2

---
+++ Seminar5_dz2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*  Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на 
+нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0  */
+
+System.Console.Write("Введите размер массива: ");
+int value = int.Parse(Console.ReadLine());
+int[] array = new int[value];
+int count = 0;
+
+int Bar(int[] array, int count)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Random random = new Random();
+        array[i] = random.Next(1, 100);
+        System.Console.Write(array[i] + "\t");
+        if (i % 2 > 0)
+            count += array[i];
+
+    }
+    return count;
+}
+int res = Bar(array, count);
+System.Console.WriteLine("\n" + res);
